1ed66cedd448139d26f00a2a5a4dbefea29e62da
unknown <seuemail@gmail.com>
2026-05-06 23:02:59 -0300

    prepara controle seguro do overlay android

--- android/app/src/main/java/com/drivelucro/app/MainActivity.java
+++ android/app/src/main/java/com/drivelucro/app/MainActivity.java
@@ -6,11 +6,27 @@ import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
+    private OverlayManager overlayManager;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
+        overlayManager = new OverlayManager(this);
+    }
 
+    public void mostrarOverlayTeste() {
+
+        if (overlayManager != null) {
+            overlayManager.mostrarOverlay("DriveLucro Overlay Ativo");
+        }
+    }
+
+    public void removerOverlayTeste() {
+
+        if (overlayManager != null) {
+            overlayManager.removerOverlay();
+        }
     }
 }
